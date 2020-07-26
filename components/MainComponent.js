c08942616a3d2d01e03d482c5bc5144cc56a358f
pavan <pavansai3008@gmail.com>
2020-07-26 18:32:42 +0530

    navigation-2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import Menu from "./MenuComponent";
 import Detail from "./DetailComponent";
 import { View, Platform } from "react-native";
-import { createStackNavigator } from "react-navigation";
-
-import { PLACES } from "../shared/content";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
+import { Icon } from "react-native-elements";
+import Home from "./HomeComponent";
 
 const MenuNavigator = createStackNavigator(
   {
@@ -25,29 +25,56 @@ const MenuNavigator = createStackNavigator(
   }
 );
 
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerTintColor: "#fff",
+    }),
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        title: "Home",
+        drawerLabel: "Home",
+      },
+    },
+    Menu: {
+      screen: MenuNavigator,
+      navigationOptions: {
+        title: "Menu",
+        drawerLabel: "Menu",
+      },
+    },
+  },
+  {
+    drawerBackgroundColor: "#D1C4E9",
+  }
+);
+
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contents: PLACES,
-      selectedPlace: null,
-    };
-  }
-
-  onPlaceSelect(placeId) {
-    this.setState({ selectedPlace: placeId });
-  }
-
   render() {
     return (
       <View
         style={{
           flex: 1,
           paddingTop:
-            Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+            Platform.OS === "andriod" ? 0 : Expo.Constants.statusBarHeight,
         }}
       >
-        <MenuNavigator />
+        <MainNavigator />
       </View>
     );
   }
